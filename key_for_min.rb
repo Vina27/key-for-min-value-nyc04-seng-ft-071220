f11d1efab45d7a31e4d28c8a8be9845717d9f8ff
Maysoon Tabali <mtab03@me.com>
2020-07-31 18:56:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,14 +12,67 @@
  # smallest_key
 #end 
 
+# def key_for_min_value(name_hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   name_hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v
+#       lowest_key = k
+#     end
+#   end
+#   lowest_key
+# end
+
+
+
+
+
 def key_for_min_value(name_hash)
-  lowest_key = nil
-  lowest_value = nil
+  lowest_key = nil 
+  lowest_value = nil 
+  
   name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
-end
+    if lowest_value == nil || v < lowest_value 
+      lowest_value = v 
+      lowest_key = k 
+  end 
+end 
+lowest_key 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
